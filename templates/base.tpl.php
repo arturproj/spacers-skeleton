a8b7f697b096aa3a7bfa5d4e5d1c312cd84a5ed2
Artur <artur.ddwebproject@gmail.com>
2024-09-03 18:59:38 +0200

    fix controller & template

--- templates/base.tpl.php
+++ templates/base.tpl.php
@@ -4,7 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?= $title ?></title>
+    <title>Hello word</title>
+    <style>
+        body {
+            display: block;
+            font-family: monospace;
+            unicode-bidi: isolate;
+            margin: 1em 0px;
+        }
+    </style>
 </head>
 
 <body>
